cebcc07449793c0eb65dc8632db733a567cb9e90
Hadiru Colley <dirudeen22@gmail.com>
2024-05-08 20:41:09 +0300

    Add a new endpoint

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000
 app.get("/", (req, res)=>{
     res.send("Welcome to a simple node application")
 })
+app.get("/hello", (req, res)=>{
+    res.send("Hello World!")
+})
 
 app.listen(port, ()=>{
     console.log("Server running at port "+ port)
