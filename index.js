c6814bf41660a2e7eb06be338b03ef10df41e5ca
Hadiru Colley <dirudeen22@gmail.com>
2024-05-08 18:15:12 +0300

    Create a express app and listen on port 3000

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to a simple node application")
+})
+
+app.listen(port, ()=>{
+    console.log("Server running at port"+ port)
+})
